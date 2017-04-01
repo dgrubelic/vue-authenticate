7205ea873c478151c96997ad3bb42e6010a12c0d
Davor Grubelic <davor.grubelic@gmail.com>
2017-04-02 00:05:51 +0200

    Fixed #2 wrong global window reference

--- src/auth.js
+++ src/auth.js
@@ -91,7 +91,7 @@ export default class VueAuthenticate {
         try { // Could be a valid JWT or an access token with the same format
           const base64Url = token.split('.')[1];
           const base64 = base64Url.replace('-', '+').replace('_', '/');
-          const exp = JSON.parse(this.$window.atob(base64)).exp;
+          const exp = JSON.parse(window.atob(base64)).exp;
           if (typeof exp === 'number') {  // JWT with an optonal expiration claims
             return Math.round(new Date().getTime() / 1000) < exp;
           }
